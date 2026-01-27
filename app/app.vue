d704ebf67a17f01302ef2951010e3545fb59ddc1
leandrocardososiva1010-hash <leandrocardososiva1010@gmail.com>
2026-01-27 00:05:41 -0300

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -10,36 +10,8 @@ useHead({
   ],
   script: [
     {
-      children: `!function (w, d, t) {
-  w.TiktokAnalyticsObject=t;
-  var ttq=w[t]=w[t]||[];
-  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
-  ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}; 
-  for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
-  ttq.instance=function(t){
-    for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);
-    return e
-  };
-  ttq.load=function(e,n){
-    var r="https://analytics.tiktok.com/i18n/pixel/events.js";
-    ttq._i=ttq._i||{};
-    ttq._i[e]=[];
-    ttq._i[e]._u=r;
-    ttq._t=ttq._t||{};
-    ttq._t[e]=+new Date;
-    ttq._o=ttq._o||{};
-    ttq._o[e]=n||{};
-    n=document.createElement("script");
-    n.type="text/javascript";
-    n.async=!0;
-    n.src=r+"?sdkid="+e+"&lib="+t;
-    e=document.getElementsByTagName("script")[0];
-    e.parentNode.insertBefore(n,e)
-  };
-
-  ttq.load('D5RTRI3C77UECCBSD1F0');
-  ttq.page();
-}(window, document, 'ttq');`
+      src: 'https://analytics.tiktok.com/i18n/pixel/events.js',
+      async: true
     }
   ],
   link: [
@@ -72,9 +44,12 @@ useSeoMeta({
 })
 
 onMounted(() => {
-  if (window.ttq) {
-    window.ttq.page()
+  if (!window.ttq) {
+    window.ttq = []
   }
+
+  window.ttq.push(['init', 'D5RTRI3C77UECCBSD1F0'])
+  window.ttq.push(['page'])
 })
 </script>
 
