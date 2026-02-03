aa32768d5dab16115295f44644d1621468801eee
Samuel151513 <flaviosamuel474@gmail.com>
2026-02-02 21:47:58 -0300

    Update utm.client.ts

--- app/plugins/utm.client.ts
+++ app/plugins/utm.client.ts
@@ -1,1 +1,22 @@
+export default defineNuxtPlugin(() => {
+  if (process.server) return
+
+  const params = new URLSearchParams(window.location.search)
+
+  if (params.toString()) {
+    localStorage.setItem('utm_params', params.toString())
+  }
+
+  const saved = localStorage.getItem('utm_params')
+
+  if (saved && !window.location.search.includes(saved)) {
+    const newUrl =
+      window.location.pathname +
+      '?' +
+      saved +
+      window.location.hash
+
+    window.history.replaceState({}, '', newUrl)
+  }
+})
 
